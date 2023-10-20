16c81d15f8a686c349e696fe1b5ce83df83bbebb
dezhamartha17 <dezhamartha17@gmail.com>
2023-10-20 09:16:42 +0700

    adding heading

--- getfunction.js
+++ getfunction.js
@@ -1,7 +1,7 @@
 import { get } from "https://jscroot.github.io/api/croot.js";
 import {setInner,addChild } from "https://jscroot.github.io/element/croot.js";
 
-export let URLGeoJson = "https://us-central1-t-decoder-401501.cloudfunctions.net/function-2";
+export let URLGeoJson = "https://us-central1-geospatial-402116.cloudfunctions.net/function-1";
 export let tableTag="tr";
 export let tableRowClass="content is-small";
 export let tableTemplate=`
